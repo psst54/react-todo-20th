f1e294730d0cc474cb2086fb9be3cef9b57c2699
psst54 <psst5491@naver.com>
2024-09-17 03:31:49 +0900

    feature: make basic ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 function App() {
-  return <div>main</div>;
+  const STATE_LIST = [
+    { title: 'Open', id: 'open' },
+    { title: 'In Progress', id: 'in-progress' },
+    { title: 'Done', id: 'done' },
+  ];
+
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <header>
+          <h1>Header</h1>
+        </header>
+        <main className="kanban-board">
+          {STATE_LIST.map((state) => (
+            <section className="kanban-board-column" id={`${state.id}-column`}>
+              <h2>{state.title}</h2>
+              <hr className={`${state.id}-hr`} />
+              <ul class="subject-list" id={`${state.id}-subject-list`}></ul>
+            </section>
+          ))}
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
