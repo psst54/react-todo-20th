0680b3b239b95bc01d5570b75d439da19fb01aab
psst54 <psst5491@naver.com>
2024-09-19 00:09:08 +0900

    feat: handle empty input submit

--- src/components/Subject/index.js
+++ src/components/Subject/index.js
@@ -3,12 +3,19 @@ import { Container, Header, Title } from './styles';
 import TaskList from 'components/TaskList';
 
 export default function Subject({ subject, deleteSubject, taskHooks }) {
+  function onDelete() {
+    if (!window.confirm('정말로 삭제하시겠습니까?')) {
+      return;
+    }
+    deleteSubject(subject.id);
+  }
+
   return (
     <Container>
       <Header>
         <Title>{subject.title}</Title>
 
-        <button onClick={() => deleteSubject(subject.id)}>
+        <button onClick={onDelete}>
           <DeleteIcon />
         </button>
       </Header>
