73c0a79be04d2ea13e010144fff1f785659ad5a9
psst54 <psst5491@naver.com>
2024-09-18 13:19:42 +0900

    feat: make subject delete button

--- src/components/Subject/index.js
+++ src/components/Subject/index.js
@@ -1,10 +1,15 @@
+import DeleteIcon from 'assets/DeleteIcon';
 import { Container, Header, Title } from './styles';
 
-export default function Subject({ subject }) {
+export default function Subject({ subject, deleteSubject }) {
   return (
     <Container>
       <Header>
         <Title>{subject.title}</Title>
+
+        <button onClick={() => deleteSubject(subject.id)}>
+          <DeleteIcon />
+        </button>
       </Header>
     </Container>
   );
